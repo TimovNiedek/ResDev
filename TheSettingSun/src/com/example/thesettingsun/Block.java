dde2fdbd62373e72cfeaa551bf8fc621494afe28
TimovNiedek <timovanniedek@me.com>
2014-04-23 15:44:14 +0200

    GameActivity gemaakt

--- TheSettingSun/src/com/example/thesettingsun/Block.java
+++ TheSettingSun/src/com/example/thesettingsun/Block.java
@@ -4,24 +4,27 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.AttributeSet;
 import android.view.SurfaceHolder;
 import android.view.View;
 
 public class Block extends View implements SurfaceHolder.Callback
 {
+	public static int SIZE = 50;
+	
 	Paint paint = new Paint();
-
-	public Block(Context context) {
+	
+	public Block(Context context, AttributeSet attrs) {
 		super(context);
 		
-		
+		System.out.println("test");
 	}
 	
 	@Override
 	public void onDraw(Canvas canvas)
 	{
 		paint.setColor(Color.GREEN);
-		canvas.drawRect(0, 0, 30, 30, paint);
+		canvas.drawRect(0, 0, SIZE, SIZE, paint);
 		
 	}
 
--- TheSettingSun/src/com/example/thesettingsun/GameActivity.java
+++ TheSettingSun/src/com/example/thesettingsun/GameActivity.java
@@ -3,7 +3,6 @@ package com.example.thesettingsun;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -12,27 +11,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
-public class MainActivity extends ActionBarActivity {
+public class GameActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-
-		//if (savedInstanceState == null) {
-		//	getSupportFragmentManager().beginTransaction()
-		//			.add(R.id.container, new PlaceholderFragment()).commit();
-		//}
+		setContentView(R.layout.activity_game);
 		
-		Intent i = new Intent(this, GameActivity.class);
-		startActivity(i);
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.game, menu);
 		return true;
 	}
 
@@ -48,5 +44,21 @@ public class MainActivity extends ActionBarActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
-	
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_game, container,
+					false);
+			return rootView;
+		}
+	}
+
 }
