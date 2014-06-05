4d58f41c24e9c5979752f5b455eb1267a6830484
DanielRoeven <daniel@roeven.com>
2014-06-05 15:50:30 +0200

    HomeTab

--- HotMeals/TabTest/gen/com/example/tabtest/R.java
+++ HotMeals/TabTest/gen/com/example/tabtest/R.java
@@ -1120,8 +1120,9 @@ containing a value of this type.
         public static final int activity_main=0x7f030017;
         public static final int fragment_layout=0x7f030018;
         public static final int fragment_search=0x7f030019;
-        public static final int recipe_view_fragment=0x7f03001a;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
+        public static final int home_layout=0x7f03001a;
+        public static final int recipe_view_fragment=0x7f03001b;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
@@ -1170,6 +1171,7 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int budget_search=0x7f0a0012;
+        public static final int greenish=0x7f0a0015;
         public static final int hello_world=0x7f0a000e;
         public static final int ingredients=0x7f0a0011;
         public static final int ingredients_search=0x7f0a0010;
