c4ab153a1910afe3186b67edebc2543cbf4e6814
DanielRoeven <daniel@roeven.com>
2014-06-06 22:48:07 +0200

    Layout stuffs

--- HotMeals/TabTest/src/com/tsjd/HotMeals/RecipeListViewAdapter.java
+++ HotMeals/TabTest/src/com/tsjd/HotMeals/RecipeListViewAdapter.java
@@ -51,7 +51,7 @@ public class RecipeListViewAdapter extends ArrayAdapter<Recipe> {
 		textView.setText(values[position].getName());		
 
 		TextView secondLine = (TextView) rowView.findViewById(R.id.secondLine);
-		secondLine.setText(values[position].getTime() + " min|�" + Recipe.doubleToCurrency(values[position].getPrice()));
+		secondLine.setText(values[position].getTime() + " min | €" + Recipe.doubleToCurrency(values[position].getPrice()));
 		
 		return rowView;
 	}
