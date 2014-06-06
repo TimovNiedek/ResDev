3e03db3548d06699c3f19c94caefae6a97d61cb6
TimovNiedek <timovanniedek@me.com>
2014-06-06 12:11:57 +0200

    BaseTabFragment update

--- HotMeals/TabTest/src/com/tsjd/HotMeals/BaseTabFragment.java
+++ HotMeals/TabTest/src/com/tsjd/HotMeals/BaseTabFragment.java
@@ -1,7 +1,32 @@
 package com.tsjd.HotMeals;
 
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 
-public class BaseTabFragment extends Fragment {
+public class BaseTabFragment extends Fragment 
+{
+	public void replaceFragment(Fragment fragment, boolean addToBackStack) {
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        if (addToBackStack) {
+            transaction.addToBackStack(null);
+        }
+        
+		FragmentManager fm = getFragmentManager();
+		FragmentTransaction ft = fm.beginTransaction();
+		ft.add(android.R.id.content, fragment);
+		ft.setCustomAnimations(R.anim.slide_in_left, android.R.anim.slide_out_right).show(fragment).commit();
+        getChildFragmentManager().executePendingTransactions();
+    }
 
+    public boolean popFragment() {
+        Log.e("test", "pop fragment: " + getChildFragmentManager().getBackStackEntryCount());
+        boolean isPop = false;
+        if (getChildFragmentManager().getBackStackEntryCount() > 0) {
+            isPop = true;
+            getChildFragmentManager().popBackStack();
+        }
+        return isPop;
+    }
 }
