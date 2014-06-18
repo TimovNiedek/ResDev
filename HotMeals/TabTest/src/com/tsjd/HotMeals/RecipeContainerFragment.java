63dde9f47e6420e47818aa43a6b8b97401bdafdb
DanielRoeven <daniel@roeven.com>
2014-06-18 11:47:14 +0200

    RecipeContainerFragment Javadoc

--- HotMeals/TabTest/src/com/tsjd/HotMeals/RecipeContainerFragment.java
+++ HotMeals/TabTest/src/com/tsjd/HotMeals/RecipeContainerFragment.java
@@ -4,7 +4,6 @@ import com.example.tabtest.R;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,11 +12,17 @@ public class RecipeContainerFragment extends BaseTabFragment
 {
 	 private boolean mIsViewInited;
 
+	 /**
+	  * Android-standard oncreate (inflate fragment)
+	  */
      @Override
      public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
          return inflater.inflate(R.layout.container_fragment, null);
      }
 
+     /**
+      * Initialize view if not yet initialized
+      */
      @Override
      public void onActivityCreated(Bundle savedInstanceState) {
          super.onActivityCreated(savedInstanceState);
@@ -27,6 +32,9 @@ public class RecipeContainerFragment extends BaseTabFragment
          }
      }
 
+     /**
+      * Initialize view with arguments from bundle
+      */
      private void initView() {
          Fragment recipeView = new RecipeView();
          Bundle arguments = this.getArguments();
