d51a8addc0b409f3c6e97de9eed05c98bcff112c
Sander van Dam <sandervandam96@gmail.com>
2014-06-18 11:04:46 +0200

    Comments added
    Added comments to RecipeListViewAdapter and RecipeListViewFragment

--- HotMeals/TabTest/src/com/tsjd/HotMeals/RecipeListViewAdapter.java
+++ HotMeals/TabTest/src/com/tsjd/HotMeals/RecipeListViewAdapter.java
@@ -1,7 +1,6 @@
 package com.tsjd.HotMeals;
 
 import android.content.Context;
-import android.content.res.Resources.NotFoundException;
 import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,47 +11,69 @@ import android.widget.TextView;
 
 import com.example.tabtest.R;
 
+/**
+ * 
+ * @author Sander van Dam
+ * @author Daniel Roeven
+ * @author Timo van Niedek
+ * @author Jaco Schalij
+ * @version 0.5
+ *
+ */
+
 public class RecipeListViewAdapter extends ArrayAdapter<Recipe> {
 	private final Context context;
 	private Recipe[] values;
-	private String path;
+	//private String path;
 	int layoutResourceID;
-	//private ArrayList<String> secondLines;
-	//private ArrayList<String> images;
 
+	/**
+	 * A simple constructor which gets the two parameters and assigns them to local variables
+	 * @param context the Context we're working in
+	 * @param recipes the array with recipes we use to make the list
+	 */
 	public RecipeListViewAdapter(Context context, Recipe[] recipes) {
 		super(context, R.layout.recipe_listview_item, recipes);
 		this.context = context;
 		this.values = recipes;
-		
-	}
 
+	}
+	
+	/**
+	 * First a RowView is initiated and then it is filled with a TextView and an ImageView
+	 * the TextViews and ImageViews are filled with an array
+	 * @return rowView
+	 */
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		LayoutInflater inflater = (LayoutInflater) context
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View rowView = inflater.inflate(R.layout.recipe_listview_item, parent, false);
+		View rowView = inflater.inflate(R.layout.recipe_listview_item, parent,
+				false);
 		TextView textView = (TextView) rowView.findViewById(R.id.label);
 		ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
-		
-		String uri = "@drawable/"+ values[position].getPath();
-		
-		
+
+		String uri = "@drawable/" + values[position].getPath();
+
 		try {
-			int imageResource = context.getResources().getIdentifier(uri, null, context.getPackageName());
+			int imageResource = context.getResources().getIdentifier(uri, null,
+					context.getPackageName());
 			Drawable res = context.getResources().getDrawable(imageResource);
 			imageView.setImageDrawable(res);
 		} catch (Exception e) {
-			int imageResource = context.getResources().getIdentifier("@drawable/non_existing_photo", null, context.getPackageName());
+			int imageResource = context.getResources().getIdentifier(
+					"@drawable/non_existing_photo", null,
+					context.getPackageName());
 			Drawable res = context.getResources().getDrawable(imageResource);
 			imageView.setImageDrawable(res);
 		}
-		
-		textView.setText(values[position].getName());		
+
+		textView.setText(values[position].getName());
 
 		TextView secondLine = (TextView) rowView.findViewById(R.id.secondLine);
-		secondLine.setText(values[position].getTime() + " min | €" + Recipe.doubleToCurrency(values[position].getPrice()));
-		
+		secondLine.setText(values[position].getTime() + " min | €"
+				+ Recipe.doubleToCurrency(values[position].getPrice()));
+
 		return rowView;
 	}
 }
--- HotMeals/TabTest/src/com/tsjd/HotMeals/RecipeListViewFragment.java
+++ HotMeals/TabTest/src/com/tsjd/HotMeals/RecipeListViewFragment.java
@@ -2,8 +2,6 @@ package com.tsjd.HotMeals;
 
 import java.util.ArrayList;
 
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -16,80 +14,110 @@ import android.widget.ListView;
 
 import com.example.tabtest.R;
 
+/**
+ * 
+ * @author Sander van Dam
+ * @author Daniel Roeven
+ * @author Timo van Niedek
+ * @author Jaco Schalij
+ * @version 0.5
+ * 
+ *          This class creates the fragment in which the RecipeList is created,
+ *          it creates a ListView and uses RecipeListViewAdapter to fill this
+ *          ListView Because RecipeListViewAdapter only accepts an array and we
+ *          have an ArrayList of the recipes, we fill an array with the contents
+ *          from the ArrayList
+ * 
+ */
 public class RecipeListViewFragment extends BaseTabFragment {
 
 	private ArrayList<Recipe> recipes;
-	
-	
+
+	/**
+	 * Standard Android function
+	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 	}
-	
+
+	/**
+	 * This function sets the view to our ListView
+	 */
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_recipelistview, container,
 				false);
 
-		
-		
-
 		return v;
 	}
-	
+
+	/**
+	 * 
+	 * When the Activity is created we send a Bundle with extra arguments
+	 * obtained with a parcelable to the fragment
+	 * 
+	 */
 	@Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        Bundle arguments = this.getArguments();
-		recipes =  arguments.getParcelableArrayList("recipes");
-		
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		Bundle arguments = this.getArguments();
+		recipes = arguments.getParcelableArrayList("recipes");
+
 		Log.d("onCreateView: RLVF", "Recipes is null is: " + (recipes == null));
 		try {
 			Log.d("onCreateView: RLVF", recipes.toString());
 		} catch (Exception e) {
 			throw new Error(e);
 		}
-		
-		Log.d("onCreateView: RLVF", "Size of arguments is: " + arguments.size() + " and contains recipes is: " + arguments.containsKey("recipes"));
+
+		Log.d("onCreateView: RLVF",
+				"Size of arguments is: " + arguments.size()
+						+ " and contains recipes is: "
+						+ arguments.containsKey("recipes"));
 		Log.d("onCreateView: RLVF", "Length of recipes is: " + recipes.size());
-		
-		final ListView listview = (ListView) getView().findViewById(R.id.listview);
-		
-		RecipeListViewAdapter adapter = new RecipeListViewAdapter(getActivity(), recipeArrayListToArray(recipes));
+
+		final ListView listview = (ListView) getView().findViewById(
+				R.id.listview);
+
+		RecipeListViewAdapter adapter = new RecipeListViewAdapter(
+				getActivity(), recipeArrayListToArray(recipes));
 		listview.setAdapter(adapter);
 		listview.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				Recipe recipe = (Recipe)listview.getItemAtPosition(position);
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				Recipe recipe = (Recipe) listview.getItemAtPosition(position);
 				Bundle arguments = new Bundle();
 				arguments.putParcelable("recipe", recipe);
 				Fragment recipeFragment = new RecipeView();
 				recipeFragment.setArguments(arguments);
-				((BaseTabFragment)getParentFragment()).replaceFragment(recipeFragment, true);
-				
-	        }
+				((BaseTabFragment) getParentFragment()).replaceFragment(
+						recipeFragment, true);
+
+			}
 		});
-    }
-	
-	private Recipe[] recipeArrayListToArray(ArrayList<Recipe> recipes)
-	{
-		
+	}
+
+	/**
+	 * Fills an array with the contents of an ArrayList
+	 * 
+	 * @param recipes the ArrayList with recipes
+	 * @return recipes an array of the param recipes 
+	 */
+	private Recipe[] recipeArrayListToArray(ArrayList<Recipe> recipes) {
+
 		Recipe[] recipesArray;
 		try {
 			recipesArray = new Recipe[recipes.size()];
 		} catch (Exception e) {
 			throw new Error(e);
 		}
-		for (int i = 0; i < recipes.size(); i++)
-		{
+		for (int i = 0; i < recipes.size(); i++) {
 			recipesArray[i] = recipes.get(i);
 		}
-		
-		return recipesArray;
-	}
 
-	public void onListItemClick(ListView l, View v, int position, long id) {
-		// do something with the data
+		return recipesArray;
 	}
 }
