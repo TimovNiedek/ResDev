6acf0ebbda118c9e5f303fd0b403f6360fe8a89b
DanielRoeven <daniel@roeven.com>
2014-06-18 11:45:23 +0200

    HomeContainerFragment Javadoc

--- HotMeals/TabTest/src/com/tsjd/HotMeals/HomeContainerFragment.java
+++ HotMeals/TabTest/src/com/tsjd/HotMeals/HomeContainerFragment.java
@@ -3,33 +3,48 @@ package com.tsjd.HotMeals;
 import com.example.tabtest.R;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+/**
+ * 
+ * @author Daniel Roeven
+ * @author Sander van Dam
+ * @author Timo van Niedek
+ * @author Jaco Schalij
+ * @version 0.5
+ *
+ */
 
 public class HomeContainerFragment extends BaseTabFragment 
 {
 	 private boolean mIsViewInited;
 
+	 /**
+	  * Initialize view
+	  */
      @Override
      public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-         Log.e("test", "tab 1 oncreateview");
          return inflater.inflate(R.layout.container_fragment, null);
      }
 
+     /**
+      * if view is not initialized, initialize
+      */
      @Override
      public void onActivityCreated(Bundle savedInstanceState) {
          super.onActivityCreated(savedInstanceState);
-         Log.e("test", "tab 1 container on activity created");
          if (!mIsViewInited) {
              mIsViewInited = true;
              initView();
          }
      }
 
+     /**
+      * Replace tab
+      */
      private void initView() {
-         Log.e("test", "tab 1 init view");
          replaceFragment(new HomeTab(), false);
      }
 }
